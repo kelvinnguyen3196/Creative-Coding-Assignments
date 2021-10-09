c2e2e5de08a9402f1e8dd549405fd8f752782b30
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2021-10-09 03:16:26 -0700

    Created moving mountains

--- assignment_3/scripts/classes/mountain.js
+++ assignment_3/scripts/classes/mountain.js
@@ -1,3 +1,42 @@
 class Mountain {
-    
+    #start;
+    #x_offset;
+    #first_point;
+    #min_height;
+    #max_height;
+    #noise_offset;
+    #bumpy;
+    #speed;
+    #color;
+
+    constructor(min_height, max_height, noise_offset, bumpy, speed, color) {
+        this.#start = 0;
+        this.#x_offset = 0;
+        this.#first_point = createVector(0, height * 100);
+        this.#min_height = min_height;
+        this.#max_height = max_height;
+        this.#noise_offset = noise_offset;
+        this.#bumpy = bumpy;
+        this.#speed = speed;
+        this.#color = color;
+    }
+
+    draw() {
+        stroke(this.#color);
+        fill(this.#color);
+
+        beginShape();
+        this.#x_offset = this.#start;
+        vertex(this.#first_point.x, this.#first_point.y);
+        for(let i = 0; i < width; i += this.#speed) {
+            let noisy_y = noise(this.#x_offset + this.#noise_offset) * height;
+            let mapped_y = map(noisy_y, 0, height, this.#min_height, this.#max_height);
+            vertex(i, mapped_y);
+            this.#x_offset += this.#bumpy;
+        }
+        vertex(width, height);
+        endShape();
+
+        this.#start += this.#bumpy;
+    }
 }
--- assignment_3/scripts/script.js
+++ assignment_3/scripts/script.js
@@ -1,9 +1,34 @@
 
+let mountains_count = 5;
+let bg_layers = new Array(mountains_count);
 
 function setup() {
-	
+	createCanvas(500, 500);
+	bg_layers[0] = new Mountain(50, 300, 40000, 0.001, 0.125, 'purple');
+	bg_layers[1] = new Mountain(150, 400, 30000, 0.001, 0.125,'yellow');
+	bg_layers[2] = new Mountain(300, 400, 20000, 0.001, 0.125, 'green');
+	bg_layers[3] = new Mountain(350, 500, 10000, 0.001, 0.125,'blue');
+	bg_layers[4] = new Mountain(450, 500, 0, 0.001, 0.125, 'red');
 }
 
 function draw() {
-	
+	background(0);
+	bg_layers[0].draw();
+	bg_layers[1].draw();
+	bg_layers[2].draw();
+	bg_layers[3].draw();
+	bg_layers[4].draw();
+
+	// beginShape();
+	// stroke(255);
+	// for(let i = 0; i < width; i++) {
+	// 	vertex(i, 200);
+	// }
+	// endShape();
+	// beginShape();
+	// stroke(255);
+	// for(let i = 0; i < width; i++) {
+	// 	vertex(i, 400);
+	// }
+	// endShape();
 }
